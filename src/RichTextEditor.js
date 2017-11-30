7cce8cffdbbeba78efbd5d821d8564f4db2b3bfa
Dillon Hafer and Ryan Messner <dev+dillonhafer+rpmessner@hashrocket.com>
2017-11-30 16:05:52 -0600

    Update to RN 50 compatibility

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import WebViewBridge from 'react-native-webview-bridge';
 import {InjectedMessageHandler} from './WebviewMessageHandler';
 import {actions, messages} from './const';
